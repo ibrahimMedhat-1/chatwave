09bf2278790af143e4ebe93ec00b270a05533c83
ibrahimmedhat <doha.kroz0@gmail.com>
2024-04-06 11:50:50 +0200

    handle navigation

--- lib/chat/chat_bubble.dart
+++ lib/chat/chat_bubble.dart
@@ -7,7 +7,6 @@ class ChatBubble extends StatelessWidget {
   final bool isUser;
   final String image;
 
-
   const ChatBubble({super.key, required this.text, required this.isUser, required this.image});
 
   @override
@@ -15,16 +14,14 @@ class ChatBubble extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
       child: Row(
-        mainAxisAlignment: isUser ? MainAxisAlignment.end : MainAxisAlignment
-            .start,
+        mainAxisAlignment: isUser ? MainAxisAlignment.end : MainAxisAlignment.start,
         children: [
           if (!isUser)
-             Padding(
+            Padding(
               padding: const EdgeInsets.only(top: 25.0),
               child: CircleAvatar(
                 backgroundColor: Colors.grey,
-                backgroundImage: NetworkImage(
-                    image),
+                backgroundImage: NetworkImage(image),
                 child: const Icon(Icons.person),
               ),
             ),
@@ -35,24 +32,21 @@ class ChatBubble extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.only(bottom: 8.0),
               child: Container(
-                constraints: BoxConstraints(maxWidth: MediaQuery
-                    .of(context)
-                    .size
-                    .width + 0.7),
+                constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width + 0.7),
                 padding: const EdgeInsets.all(10),
                 decoration: BoxDecoration(
                     color: isUser ? Colors.grey : Colors.green,
                     borderRadius: !isUser
                         ? const BorderRadius.only(
-                      topRight: Radius.circular(15),
-                      topLeft: Radius.circular(15),
-                      bottomRight: Radius.circular(15),
-                    )
+                            topRight: Radius.circular(15),
+                            topLeft: Radius.circular(15),
+                            bottomRight: Radius.circular(15),
+                          )
                         : const BorderRadius.only(
-                      bottomRight: Radius.circular(15),
-                      topLeft: Radius.circular(15),
-                      bottomLeft: Radius.circular(15),
-                    )),
+                            bottomRight: Radius.circular(15),
+                            topLeft: Radius.circular(15),
+                            bottomLeft: Radius.circular(15),
+                          )),
                 child: Text(
                   text,
                   style: const TextStyle(color: Colors.white),
@@ -61,29 +55,27 @@ class ChatBubble extends StatelessWidget {
             ),
           ),
           if (isUser)
-             Padding(
+            Padding(
               padding: const EdgeInsets.only(bottom: 40.0, left: 5),
               child: userModel!.image == ''
                   ? const Material(
-                elevation: 4,
-                shape: CircleBorder(),
-                child: CircleAvatar(
-
-                  backgroundImage: AssetImage(
-                      'assets/images/profile.png'),
-                ),
-              )
+                      elevation: 4,
+                      shape: CircleBorder(),
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage('assets/images/profile.png'),
+                      ),
+                    )
                   : Material(
-                elevation: 4,
-                shape: const CircleBorder(),
-                child: CircleAvatar(
-                  backgroundColor: Colors.black,
-                  backgroundImage: NetworkImage(
-                      userModel!.image.toString()),
-                ),
-              ),
+                      elevation: 4,
+                      shape: const CircleBorder(),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.black,
+                        backgroundImage: NetworkImage(userModel!.image.toString()),
+                      ),
+                    ),
             ),
         ],
       ),
     );
-  }}
+  }
+}
